6bedbbed63b5829e5c9ba8336b25bdd7ea16a7fd
toticavalcanti <toticavalcanti@hotmail.com>
2023-05-23 19:11:54 -0300

    31-closing-channels

--- 31 - closing-channels/non_blocking_channel_operations.go
+++ 31 - closing-channels/non_blocking_channel_operations.go
@@ -1,37 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	// Criando um canal sem bloqueio para atualização de estoque
-	stockUpdateCh := make(chan int, 1)
+	jobs := make(chan int, 5)
+	done := make(chan bool)
 
-	// Iniciando uma goroutine para processar as atualizações de estoque
 	go func() {
-		for productId := range stockUpdateCh {
-			// Simulando uma atualização de estoque
-			fmt.Printf("Atualizando estoque do produto %d...\n", productId)
-			time.Sleep(2 * time.Second)
-			fmt.Printf("Estoque do produto %d atualizado\n", productId)
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("Trabalho recebido:", j)
+			} else {
+				fmt.Println("Todos os trabalhos recebidos")
+				done <- true
+				return
+			}
 		}
 	}()
 
-	// Realizando uma venda e atualizando o estoque do produto
-	productId := 123
-	fmt.Printf("Realizando venda do produto %d...\n", productId)
-	time.Sleep(time.Second)
-
-	// Enviando uma mensagem para o canal sem bloqueio de atualização de estoque
-	select {
-	case stockUpdateCh <- productId:
-		fmt.Printf("Mensagem de atualização de estoque para o produto %d enviada com sucesso\n", productId)
-	default:
-		fmt.Printf("Falha ao enviar mensagem de atualização de estoque para o produto %d\n", productId)
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("Trabalho enviado:", j)
 	}
+	close(jobs)
+	fmt.Println("Todos os trabalhos enviados")
 
-	// Finalizando a venda
-	fmt.Printf("Venda do produto %d realizada com sucesso\n", productId)
+	<-done
 }
